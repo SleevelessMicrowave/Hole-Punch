fa322d8314d7a9bd95a940f3552250533199ca12
SleevelessMicrowave <62040052+SleevelessMicrowave@users.noreply.github.com>
2020-06-11 21:30:43 -0400

    wtf

---
+++ Hole Punch/Assets/Code/bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class bullet : MonoBehaviour
+{
+
+    private Vector3 shootDir;
+
+    public void Setup(Vector3 shootDir)
+    {
+        this.shootDir = shootDir;
+        transform.eulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(shootDir));
+        Destroy(gameObject, 5f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveSpeed = 100f;
+        transform.position += shootDir * moveSpeed * Time.deltaTime;
+
+        float hitDetectionSize = 3f;
+        Target target = Target.getClosest 
+    }
+}
