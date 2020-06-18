5d9e48d516c114963b2086fa8fabafa38b06a5b0
Jonpaco231 <62667292+Jonpaco231@users.noreply.github.com>
2020-06-18 15:04:35 -0400

    shooting

---
+++ Hole Punch/Assets/Code/Hit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        Destroy(gameObject, 10f);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+
+        // Rotate the object so that the y-axis faces along the normal of the surface
+        Destroy(gameObject);
+    }
+}
---
+++ Hole Punch/Assets/Code/Shoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.Versioning;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Shoot: MonoBehaviour
+{
+
+    GameObject prefab;
+    public float speed = 100f;
+
+    public KeyCode shoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefab = Resources.Load("bullet") as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(shoot))
+        {
+            GameObject instBullet = Instantiate(prefab, transform.position+Camera.main.transform.forward*5+Camera.main.transform.up*3/2, Quaternion.Euler(Camera.main.transform.eulerAngles.x, Camera.main.transform.eulerAngles.y, Camera.main.transform.eulerAngles.z)) as GameObject;
+            Rigidbody instBulletRigidbody = instBullet.GetComponent<Rigidbody>();
+            instBulletRigidbody.velocity = Quaternion.Euler(Camera.main.transform.eulerAngles.x, Camera.main.transform.eulerAngles.y, Camera.main.transform.eulerAngles.z) * new Vector3(0, 0, speed);
+            //instBulletRigidbody.AddForce(Vector3.left * speed);
+            //Camera.main.transform.forward * speed
+        }
+    }
+}
