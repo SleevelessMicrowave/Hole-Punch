6400b4c6a9048166b1808ce307f4bec50c18fffc
SupermanS101 <62188988+SupermanS101@users.noreply.github.com>
2020-12-06 20:47:59 -0500

    Comments
    Comments

--- Hole Punch/Assets/Code/MouseLook.cs
+++ Hole Punch/Assets/Code/MouseLook.cs
@@ -15,19 +15,26 @@ public class MouseLook : MonoBehaviour
     {
         
         Cursor.visible = false;
+        //wcursor dissapear and locks in place for like movement stuff
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //sensivity * mouse movement x and y inputs  
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        //
         xRotation -= mouseY;
+        //stops rotation so that you can only look straight up and down for a certain angle
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
+        //on the transform it changes the rotation based on where lookinh
+        //if not attached to anything attached to camera object bc script is attached to the object so  rotates the camera
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        //rotates the first person player so the plaer movement works. This changes the transform on the player 
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
--- Hole Punch/Assets/Code/PlayerMovement.cs
+++ Hole Punch/Assets/Code/PlayerMovement.cs
@@ -1,11 +1,17 @@
-﻿using System.Collections;
+﻿//variables pulled and linked through unity 
+//axis is attached to guy
+using System.Collections;
 using System.Collections.Generic;
+//unity libraries
 using UnityEngine;
+//link to unity
 
 public class PlayerMovement : MonoBehaviour
 {
+    //t5ransform into object
     [SerializeField] private Transform debughitPointtransform;
 
+    //controller for characteer
     public CharacterController controller;
 
     public GameObject character;
@@ -15,23 +21,29 @@ public class PlayerMovement : MonoBehaviour
 
     public ParticleSystem wind;
 
+//decimal vars
     public static float speed = 8f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
 
+//keycode var but set equal to a key 
     public KeyCode sprint;
     public KeyCode crouch;
 
+//attatch a transform for positions
     public Transform groundCheck;
     public float groundDistance = 0.4f;
+    //object that you can assign to layers
     public LayerMask groundMask;
 
+//3d vectpr
     Vector3 velocity;
     bool isGrounded;
 
+//state you're in
     private State state;
     private Vector3 hookshotPosition;
-
+//sets up two states
     private enum State
     {
         Normal,
@@ -64,6 +76,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void MovePlayer()
     {
+        //sprint key down
         /*if (Input.GetKeyDown(sprint))
         {
             speed = 12;
@@ -78,26 +91,29 @@ public class PlayerMovement : MonoBehaviour
             wind.enableEmission = false;
             wind.Stop();
         }*/
-
+//checks if player is on the ground and touching. Makes sure you are on the ground at all times
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
-
+    //which direction you are facing returns from -1 to 1 wasd
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
+    
+    //transform.right says move along the x axis * direction * the speed
         Vector3 move = transform.right * x * speed + transform.forward * z * speed + transform.up * velocity.y;
-
+        //jump is equal to spacebar pressd. If on ground jump
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
+    //every frame time in seconds since last frame so it's more accurate if franme rate changes 
         velocity.y += gravity * Time.deltaTime;
 
+    //move method of charactar ontroller runs the move vector3
         controller.Move(move * Time.deltaTime);
 
         /*if (Input.GetKeyDown(crouch))
@@ -114,11 +130,14 @@ public class PlayerMovement : MonoBehaviour
         }*/
     }
 
+    //if e is pressed 
     private void HandleHookshotStart()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
             //Checks if raycast hits something from origin, direction, and boolean if something is hit
+            //raycast shoots invisible line from point A to point B 
+            //If something is detected in RaycasHit raycastHit where the player is looking from the players position 
             if (Physics.Raycast(player.transform.position, player.transform.forward, out RaycastHit raycastHit)){
                 debughitPointtransform.position = raycastHit.point; //places object at the point when raycast hits it
                 hookshotPosition = raycastHit.point;
@@ -129,6 +148,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void HandlehookshotMovement()
     {
+        //hookshopt position and - location of player to figure the distance to move 
         Vector3 hookshotDir = (hookshotPosition - transform.position).normalized;
 
         float hookshotSpeed = Vector3.Distance(transform.position, hookshotPosition);
@@ -137,11 +157,13 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(hookshotDir * hookshotSpeed * hookshotSpeedMultiplier * Time.deltaTime);
 
         float reachedHookshotPositionDistance = 3f;
+        //distance between orb and person cant go below three, then goes back to normal no teleporting state
         if (Vector3.Distance(transform.position, hookshotPosition) < reachedHookshotPositionDistance)
         {
             //reached hookshot position
             state = State.Normal;
         }
+        //if click e again stop immideatly
         else if (Input.GetKeyDown(KeyCode.E))
         {
             state = State.Normal;
